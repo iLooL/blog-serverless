30328578eba93f71bd90ecd4c73dd383806f41b0
Adam Gumieniak <Ruckas_x1@hotmail.com>
2022-02-17 18:57:18 -0500

    added modal to delete button

--- src/App.js
+++ src/App.js
@@ -4,10 +4,31 @@ import SignUp from './Components/Registration/SignUp';
 import Login from './Components/Registration/Login';
 import axios from 'axios';
 
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
 function App() {
   // api configs
   const apiUrl = 'https://aqt15rrwbl.execute-api.us-east-1.amazonaws.com/posts';
   const [posts, setPosts] = useState([]);
+
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   const getApi = async() => {
     try {
@@ -21,8 +42,9 @@ function App() {
   const deletePost = async(e) => {
     const id = e.target.getAttribute('name');
     try {
-      const reponse = await axios.delete(`${apiUrl}/${id}`);
-      console.log(reponse);
+      await axios.delete(`${apiUrl}/${id}`);
+      handleClose();
+      getApi();
     } catch(err) {
       console.log(err);
     }
@@ -44,7 +66,21 @@ function App() {
             { post.title }
           </Link>
             <p>{ post.author }</p>
-            <a href="/" name={post.id} onClick={deletePost}>Delete</a>
+            <div>
+              <Button onClick={handleOpen}>Delete</Button>
+              <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description">
+                <Box sx={style}>
+                  <Typography id="modal-modal-description" sx={{ mt: 2 }}>Are you sure you want to delete?</Typography>
+                  <Button name={post.id} onClick={deletePost}>
+                    Delete
+                  </Button>
+                </Box>
+              </Modal>
+            </div> 
         </div>
       ))}
     </div>
